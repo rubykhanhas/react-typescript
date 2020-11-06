93676947e218572c8318973f33779a224cfe4a41
linux <rubykhanhas@gmail.com>
2020-11-06 17:12:53 +0700

    - old layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React, { Fragment } from 'react'
-import './App.scss';
-import Col from '@/Layouts/Grid/Col';
-import Row from '@/Layouts/Grid/Row';
+import React, { Fragment } from "react";
+import "./App.scss";
 
 function App() {
     return (
         <Fragment>
-            <Row>
-                <Col span={12}>
-                    <h2>React Typescript Skeleton</h2>
-                    <h5>rubykhanhas@gmail.com</h5>
-                </Col>
-            </Row>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 col-md-12">
+                        <h2>React Typescript Skeleton</h2>
+                    </div>
+                    <div className="col-6 col-md-12">
+                        <h5>rubykhanhas@gmail.com</h5>
+                    </div>
+                </div>
+            </div>
         </Fragment>
-    )
+    );
 }
 
 export default App;
